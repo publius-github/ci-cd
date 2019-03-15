4f05d5a88891c4bd3cf23f42c866ffba0c93bf96
Siarhei Zverau <siarhei.zverau@cloudsolutions.co.uk>
2019-03-15 12:52:58 +0300

    added ecs

--- terraform-ecs/99-vars.tf
+++ terraform-ecs/99-vars.tf
@@ -1,3 +1,10 @@
+variable "aws_access_key" {
+  type = "string"
+}
+variable "aws_secret_key" {
+  type = "string"
+}
+
 variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "us-east-1"
