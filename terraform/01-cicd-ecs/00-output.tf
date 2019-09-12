065e4fc0e0ec774403c0aa2fa38cfe2ece278918
Siarhei Zverau <siarhei.zverau@cloudsolutions.co.uk>
2019-09-12 16:23:26 +0300

    commit

--- terraform/01-cicd-ecs/00-output.tf
+++ terraform/01-cicd-ecs/00-output.tf
@@ -1,11 +1,11 @@
-output "alb_hostname" {
+output "alb_backend" {
   value = "${aws_alb.alb_backend.dns_name}"
 }
 
-output "alb_hostname" {
+output "alb_frontend" {
   value = "${aws_alb.alb_frontend.dns_name}"
 }
 
-output "alb_hostname" {
+output "alb_db" {
   value = "${aws_alb.alb_db.dns_name}"
 }
