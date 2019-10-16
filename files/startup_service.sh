d31ca72eaf4e586af4c81425cc80320631523453
Aleksandr Chudilovski <aleksandr.chudilovski@bglgroup.co.uk>
2019-10-16 17:22:48 +0300

    add cicd-service

--- files/startup_service.sh
+++ files/startup_service.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-cd /etc/ecosystem
+cd /data/docker
 
-if [ -f /etc/default/ecosystem ]; then
-  . /etc/default/ecosystem
-fi
-. ${PWD}/env.sh
-exec /usr/local/bin/docker-compose up -t2 >> /var/log/ecosystem/ecosystem.log 2>&1
+exec /usr/local/bin/docker-compose up
